b75c050207b288f312f4745cdeb43542850abcb1
MT <martin.tatarski@gmail.com>
2023-10-09 15:49:26 +0100

    Added javascript code

---
+++ assets/script.js
@@ -0,0 +1,19 @@
+// const topic = "HTML";
+
+// if(topic === "HTML"){
+//   console.log("let's study HTML!")
+// }else if(topic === "CSS"){
+//   console.log("let's study CSS!")  
+// }else if(topic === "Git"){
+//   console.log("let's study Git!")  
+// }else if(topic === "Javascipt"){
+//   console.log("let's study Javascrpt!")  
+// }else {
+//   console.log("Please try again");
+// }
+
+var topics = ['HTML', 'CSS', 'Git', 'JavaScript'];
+
+for(let i = 0; i < topics.length; i++){
+  console.log(topics[i])
+}
